3a00a805c38bf15de446b7dbd6a73656ca312d22
Shio <83135507+Shio1025@users.noreply.github.com>
2023-12-10 18:07:50 +0400

    Add files via upload

---
+++ CI-CD/CI-CD/CI_CDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_CDApp.swift
+//  CI-CD
+//
+//  Created by Shio Birbichadze on 10.12.23.
+//
+
+import SwiftUI
+
+@main
+struct CI_CDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
