772fdf736c665d0f8ae51fcbd34b900d09e048e4
Shio Birbichadze <sbirbichadze@bog.ge>
2023-12-10 20:13:49 +0400

    add app center

--- CI-CD/CI-CD/CI_CDApp.swift
+++ CI-CD/CI-CD/CI_CDApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import AppCenter
+import AppCenterCrashes
+import AppCenterAnalytics
 
 @main
 struct CI_CDApp: App {
+    init() {
+//        AppCenter.start(withAppSecret: "030b03b4-3286-4e13-92fd-6b7637c85144", services: [Analytics.self, Crashes.self])
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
--- CI-CD/CI-CD/ContentView.swift
+++ CI-CD/CI-CD/ContentView.swift
@@ -6,8 +6,19 @@
 //
 
 import SwiftUI
+import AppCenterCrashes
+import AppCenterAnalytics
 
 struct ContentView: View {
+    
+    init() {
+        AppCenter.start(withAppSecret: "030b03b4-3286-4e13-92fd-6b7637c85144", services: [Analytics.self, Crashes.self])
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//            Crashes.generateTestCrash()
+            Analytics.trackEvent("Welcome Page")
+        }
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
